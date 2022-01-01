d3fcfa5709a358200dadca7776984e6200a73ca1
pvphat19 <64125852+pvphat19@users.noreply.github.com>
2022-01-01 22:16:21 +0700

    comment

--- routers/post.js
+++ routers/post.js
@@ -4,6 +4,7 @@ const {getUser} = require('../middleware');
 const postRouter = express.Router();
 const postResolver = require('../resolvers/post');
 
-postRouter.get('/uploadpost', postResolver.uploadPost)
+postRouter.get('/uploadpost', postResolver.uploadPost);
+postRouter.get('/commentpost', postResolver.commentPost)
 
 module.exports = postRouter;
